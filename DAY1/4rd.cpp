6fca8066393af7d4096bc48cc890141c10de9326
Saurabh Kumar <saurabh.kumar288205@gmail.com>
2026-06-01 20:01:42 +0530

    forth prog

---
+++ DAY1/4rd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n,count=0;
+    cout<<"Enter a no.";
+    cin>>n;
+    while(n>0){
+
+        n%10;
+        count++;
+        n=n/10;
+        
+    }
+    cout<<"No. of digits are :"<<count;
+
+    return 0;
+}
+

3a574d7ef62e426f580993d6348e564061d73dd1
Saurabh Kumar <saurabh.kumar288205@gmail.com>
2026-06-01 19:52:44 +0530

    prog third

---
+++ DAY1/3rd.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+    int n,i,f=1;
+    cout<<"enter a no. whose factorial :";
+    cin>>n;
+    i=1;
+    if (n<0)
+    
+    cout<<"factorial is not defined ";
+
+    else if(n=0)
+    cout<<"factorial is equal to : 1";
+
+    else{
+    for(i;i<=n;i++){
+        f=f*i;
+    }
+}
+cout<<"factorial is :"<<f;
+ 
+
+}

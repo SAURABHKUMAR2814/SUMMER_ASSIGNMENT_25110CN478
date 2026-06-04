d426e914f672377cd27d449e886bab06aff20a61
Saurabh Kumar <saurabh.kumar288205@gmail.com>
2026-06-04 21:38:37 +0530

    day 4 prog 2

--- DAY 4/13.cpp
+++ DAY 4/13.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() 
-{
-    int n, a = 0, b = 1, c;
+// int main() 
+// {
+//     int n, a = 0, b = 1, c;
 
-    cout << "Enter number of terms: ";
-    cin >> n;
+//     cout << "Enter number of terms: ";
+//     cin >> n;
 
-    cout << "Fibonacci Series: ";
+//     cout << "Fibonacci Series: ";
 
-    for(int i = 1; i <= n; i++)
-    {
-        cout << a << " ";
+//     for(int i = 1; i <= n; i++)
+//     {
+//         cout << a << " ";
 
-        c = a + b;
-        a = b;
-        b = c;
-    }
+//         c = a + b;
+//         a = b;
+//         b = c;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ DAY 4/14.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a = 0, b = 1, c;
+
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    if(n == 1)
+        cout << "Nth Fibonacci term is: " << a;
+    
+    else if(n == 2)
+        cout << "Nth Fibonacci term is: " << b;
+
+    else
+    {
+        for(int i = 3; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        cout << "Nth Fibonacci term is: " << c;
+    }
+
+    return 0;
+}
